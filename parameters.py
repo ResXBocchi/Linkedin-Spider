8e5df554455c79642dd9afaa510fa74d19ef3dc9
unknown <bocchi.dev@gmail.com>
2020-12-30 14:50:46 -0300

    implementing improvements suggested by @heitordeep

--- parameters.py
+++ parameters.py
@@ -1,7 +1,5 @@
+from decouple import config
 search_query = 'site:linkedin.com/in AND "python developer" AND "Alagoas"'
-result_file = 'output.csv'
-
-username='insira seu username'
-password ='insira sua senha'
-
-
+username = config('EMAIL')
+password = config('PASSWORD')
+driver_path = config('DRIVER_PATH')
